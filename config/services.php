3e448408a04bddfbdf62f5fc43970c18bd143523
Angel Acevedo <angelacevedo975@gmail.com>
2020-09-29 17:42:16 -0500

    adding names

--- config/services.php
+++ config/services.php
@@ -31,8 +31,8 @@ return [
     ],
 
     'facebook' => [
-        'client_id' => env('GITHUB_CLIENT_ID'),
-        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => '/login/facebook/callback',
     ],
 
