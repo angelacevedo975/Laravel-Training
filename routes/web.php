fd632f302e2cdbddde954e3319c46df1b214d0c8
Angel Acevedo <angelacevedo975@gmail.com>
2020-09-29 10:00:01 -0500

    use carbon to show dates

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $mytime = Carbon\Carbon::now();
+    return view('home',["date"=>$mytime->toDateTimeString()]);
     
 });
 
